c36f7b236d48f436692fdeb80fd1c1ae2ef1e714
HanChuenFoong <47974238+HanChuenFoong@users.noreply.github.com>
2022-12-27 19:24:25 -0800

    Add files via upload

---
+++ cooking-ninja/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useTheme } from './hooks/useTheme';
+
+import Navbar from './components/Navbar';
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+import ThemeSelector from './components/ThemeSelector';
+
+function App() {
+  const { mode } = useTheme()
+
+  return (
+    <div className={`App ${mode}`}>
+      <BrowserRouter>
+        <Navbar />
+        <ThemeSelector />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
